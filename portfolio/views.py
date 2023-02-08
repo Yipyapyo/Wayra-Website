e57558f0b5f42111198a963cd399b9a3ef74fdb5
Rebi.boy <rebjok911@gmail.com>
2023-02-08 20:28:37 +0000

    added the initial frontend for the dashboard

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def dashboard(request):
+    '''The main dashboard page of the website.'''
+    counter = {1,2,3,4,5,6,7,8,9}
+    return render(request, 'main_dashboard.html', {'counter':counter})
