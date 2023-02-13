9c118890debb1b1698695d8ff3e902d6759a82ec
AdnanTurkay <adntrky@gmail.com>
2023-02-13 20:57:10 +0000

    Fix tests.

--- portfolio/tests/models/test_individual.py
+++ portfolio/tests/models/test_individual.py
@@ -1,17 +1,15 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
-from portfolio.models import individual_model
+from portfolio.models import Individual
 from phonenumber_field.phonenumber import PhoneNumber
-
 
 
 class IndividualTests(TestCase):
     """Unit tests for the individual model."""
 
-
     # Sets up example individual to be used for tests
     def setUp(self):
-        self.individual = individual_model.objects.create(
+        self.individual = Individual.objects.create(
              AngelListLink = "https://www.AngelList.com",
              CrunchbaseLink = "https://www.Crunchbase.com",
              LinkedInLink = "https://www.LinkedIn.com",
@@ -22,93 +20,75 @@ class IndividualTests(TestCase):
              SecondaryNumber = PhoneNumber.from_string("+441325777655")
         )
 
-
     # Tests if individual is valid
     def test_valid_individual(self):
         self._assert_individual_is_valid()
-
 
     def test_angellistlink_cannot_be_blank(self):
         self.individual.AngelListLink = ''
         self._assert_individual_is_invalid()
 
-
     def test_angellistlink_can_be_200_characters_long(self):
         self.individual.AngelListLink = "https://www.google.com/search?q=created+a+lot+of+things+for+all+of+my+friends+andajv&oq=created+a+lot+of+things+for+all+of+my+friends+andajv&aqs=chrome..69i57j69i59l4.483j0j9&sourcesid=chrome&ie=UTF-8"
         self._assert_individual_is_valid()
-
 
     def test_angellistlink_cannot_be_over_200_characters_long(self):
         self.individual.AngelListLink = "https://www.google.com/search?q=generate+a+url+with+200+characters+long+long+long&sxsrf=AJOqlzUy02dw8aZyAEwLl5Bkc6A_o8sCuQ%3A1676293616530&ei=8DXqY7OJIJH_gAaIzZKABQ&ved=0ahUKEwizwbWsyJL9AhWRP8AKHYimBFAQ4dUDCA8&uact=5&oq=generate+a+url+with+200+characters+long+long+long&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIFCCEQoAE6CggAEEcQ1gQQsAM6BwghEKABEApKBAhBGABKBAhGGABQigJYyRBgoBNoAXABeACAAWKIAdEHkgECMTWYAQCgAQHIAQrAAQE&sclient=gws-wiz-serp"
         self._assert_individual_is_invalid()
 
-
     def test_crunchbaselink_cannot_be_blank(self):
         self.individual.CrunchbaseLink = ''
         self._assert_individual_is_invalid()
-
 
     def test_crunchbaselink_can_be_200_characters_long(self):
         self.individual.CrunchbaseLink = "https://www.google.com/search?q=created+a+lot+of+things+for+all+of+my+friends+andajv&oq=created+a+lot+of+things+for+all+of+my+friends+andajv&aqs=chrome..69i57j69i59l4.483j0j9&sourcesid=chrome&ie=UTF-8"
         self._assert_individual_is_valid()
 
-
     def test_crunchbaselink_cannot_be_over_200_characters_long(self):
         self.individual.CrunchbaseLink = "https://www.google.com/search?q=generate+a+url+with+200+characters+long+long+long&sxsrf=AJOqlzUy02dw8aZyAEwLl5Bkc6A_o8sCuQ%3A1676293616530&ei=8DXqY7OJIJH_gAaIzZKABQ&ved=0ahUKEwizwbWsyJL9AhWRP8AKHYimBFAQ4dUDCA8&uact=5&oq=generate+a+url+with+200+characters+long+long+long&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIFCCEQoAE6CggAEEcQ1gQQsAM6BwghEKABEApKBAhBGABKBAhGGABQigJYyRBgoBNoAXABeACAAWKIAdEHkgECMTWYAQCgAQHIAQrAAQE&sclient=gws-wiz-serp"
         self._assert_individual_is_invalid()
-
 
     def test_linkedinlink_cannot_be_blank(self):
         self.individual.LinkedInLink = ''
         self._assert_individual_is_invalid()
 
-
     def test_linkedinlink_can_be_200_characters_long(self):
         self.individual.LinkedInLink = "https://www.google.com/search?q=created+a+lot+of+things+for+all+of+my+friends+andajv&oq=created+a+lot+of+things+for+all+of+my+friends+andajv&aqs=chrome..69i57j69i59l4.483j0j9&sourcesid=chrome&ie=UTF-8"
         self._assert_individual_is_valid()
-
 
     def test_linkedinlink_cannot_be_over_200_characters_long(self):
         # self.individual.LinkedInLink = "https://www." + ("x" * 185) + ".com"
         self.individual.LinkedInLink = 'https://www.google.com/search?q=generate+a+url+with+200+characters+long+long+long&sxsrf=AJOqlzUy02dw8aZyAEwLl5Bkc6A_o8sCuQ%3A1676293616530&ei=8DXqY7OJIJH_gAaIzZKABQ&ved=0ahUKEwizwbWsyJL9AhWRP8AKHYimBFAQ4dUDCA8&uact=5&oq=generate+a+url+with+200+characters+long+long+long&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIFCCEQoAE6CggAEEcQ1gQQsAM6BwghEKABEApKBAhBGABKBAhGGABQigJYyRBgoBNoAXABeACAAWKIAdEHkgECMTWYAQCgAQHIAQrAAQE&sclient=gws-wiz-serp'
         self._assert_individual_is_invalid()
 
-
     def test_company_cannot_be_blank(self):
         self.individual.Company = ''
         self._assert_individual_is_invalid()
-
 
     def test_company_can_be_100_characters_long(self):
         self.individual.Company = 'x' * 100
         self._assert_individual_is_valid()
 
-
     def test_company_cannot_be_over_100_characters_long(self):
         self.individual.Company = 'x' * 101
         self._assert_individual_is_invalid()
-
 
     def test_position_cannot_be_blank(self):
         self.individual.Position = ''
         self._assert_individual_is_invalid()
 
-
     def test_position_can_be_100_characters_long(self):
         self.individual.Position = 'x' * 100
         self._assert_individual_is_valid()
-
 
     def test_position_cannot_be_over_100_characters_long(self):
         self.individual.Position = 'x' * 101
         self._assert_individual_is_invalid()
 
-
     # Tests the fact that the email field of a individual shouldn't be blank
     def test_email_must_not_be_blank(self):
         self.individual.Email = ''
         self._assert_individual_is_invalid()
-
 
     # Tests the fact that the email field of a individual can be not unique
     def test_email_can_be_not_unique(self):
@@ -116,16 +96,13 @@ class IndividualTests(TestCase):
         self.individual.Email = second_individual.Email
         self._assert_individual_is_valid()
 
-
     def test_primarynumber_must_not_be_blank(self):
         self.individual.PrimaryNumber = ''
         self._assert_individual_is_invalid()
 
-
     def test_secondarynumber_can_be_blank(self):
         self.individual.SecondaryNumber = ''
         self._assert_individual_is_valid()
-
 
     """Helper functions"""
 
@@ -136,23 +113,21 @@ class IndividualTests(TestCase):
         except ValidationError:
             self.fail('Test individual is not valid.')
 
-
     # Assert a individual is invalid
     def _assert_individual_is_invalid(self):
         with self.assertRaises(ValidationError):
             self.individual.full_clean()
 
-
     # Create a second individual
     def _create_second_individual(self):
-        individual = individual_model.objects.create(
-             AngelListLink = "www.AngelList2.com",
-             CrunchbaseLink = "www.Crunchbase2.com",
-             LinkedInLink = "www.LinkedInLink2.com",
-             Company = "exampleCompany2",
-             Position = "examplePosition2",
-             Email = "test2@gmail.com",
-             PrimaryNumber = PhoneNumber.from_string("+447975777662"),
-             SecondaryNumber = PhoneNumber.from_string("+441325777651")
+        individual = Individual.objects.create(
+             AngelListLink="www.AngelList2.com",
+             CrunchbaseLink="www.Crunchbase2.com",
+             LinkedInLink="www.LinkedInLink2.com",
+             Company="exampleCompany2",
+             Position="examplePosition2",
+             Email="test2@gmail.com",
+             PrimaryNumber=PhoneNumber.from_string("+447975777662"),
+             SecondaryNumber=PhoneNumber.from_string("+441325777651")
         )
         return individual
--- portfolio/tests/models/test_residential_adress.py
+++ portfolio/tests/models/test_residential_adress.py
@@ -1,10 +1,9 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
-from portfolio.models import individual_model
-from portfolio.models import residentialAddress
+from portfolio.models import Individual
+from portfolio.models import ResidentialAddress
 from phonenumber_field.phonenumber import PhoneNumber
 from django_countries.fields import Country
-
 
 
 class ResidentialAdressTests(TestCase):
@@ -13,7 +12,7 @@ class ResidentialAdressTests(TestCase):
 
     # Sets up example individual to be used for tests
     def setUp(self):
-        self.individual = individual_model.objects.create(
+        self.individual = Individual.objects.create(
              AngelListLink = "https://www.AngelList.com",
              CrunchbaseLink = "https://www.Crunchbase.com",
              LinkedInLink = "https://www.LinkedIn.com",
@@ -24,7 +23,7 @@ class ResidentialAdressTests(TestCase):
              SecondaryNumber = PhoneNumber.from_string("+441325777655")
         )
 
-        self.residential_adress = residentialAddress.objects.create(
+        self.residential_adress = ResidentialAddress.objects.create(
             address_line1 = "testAdress1",
             address_line2 = "testAdress2",
             postal_code = "testCode",
@@ -34,90 +33,72 @@ class ResidentialAdressTests(TestCase):
             individual = self.individual
             )
 
-
     def test_valid_residential_adress(self):
         self._assert_residential_adress_is_valid()
-
 
     def test_address_line1_cannot_be_blank(self):
         self.residential_adress.address_line1 = ''
         self._assert_residential_adress_is_invalid()
 
-
     def test_address_line1_can_be_50_characters_long(self):
         self.residential_adress.address_line1 = 'x' * 50
         self._assert_residential_adress_is_valid()
-
 
     def test_address_line1_cannot_be_over_50_characters_long(self):
         self.residential_adress.address_line1 = 'x' * 51
         self._assert_residential_adress_is_invalid()
 
-
     def test_address_line2_can_be_blank(self):
         self.residential_adress.address_line2 = ''
         self._assert_residential_adress_is_valid()
-
 
     def test_address_line2_can_be_50_characters_long(self):
         self.residential_adress.address_line2 = 'x' * 50
         self._assert_residential_adress_is_valid()
 
-
     def test_address_line2_cannot_be_over_50_characters_long(self):
         self.residential_adress.address_line2 = 'x' * 51
         self._assert_residential_adress_is_invalid()
-
 
     def test_postal_code_can_be_10_characters_long(self):
         self.residential_adress.postal_code = 'x' * 10
         self._assert_residential_adress_is_valid()
 
-
     def test_postal_code_cannot_be_over_10_characters_long(self):
         self.residential_adress.postal_code = 'x' * 11
         self._assert_residential_adress_is_invalid()
-
 
     def test_city_cannot_be_blank(self):
         self.residential_adress.city = ''
         self._assert_residential_adress_is_invalid()
 
-
     def test_city_can_be_50_characters_long(self):
         self.residential_adress.city = 'x' * 50
         self._assert_residential_adress_is_valid()
-
 
     def test_city_cannot_be_over_50_characters_long(self):
         self.residential_adress.city = 'x' * 51
         self._assert_residential_adress_is_invalid()
 
-
     def test_state_can_be_blank(self):
         self.residential_adress.state = ''
         self._assert_residential_adress_is_valid()
-
 
     def test_state_can_be_50_characters_long(self):
         self.residential_adress.state = 'x' * 50
         self._assert_residential_adress_is_valid()
 
-
     def test_state_cannot_be_over_50_characters_long(self):
         self.residential_adress.state = 'x' * 51
         self._assert_residential_adress_is_invalid()
-
 
     def test_country_has_to_be_valid(self):
         self.residential_adress.country = Country("ababa")
         self._assert_residential_adress_is_invalid()
 
-
     def test_individual_cannot_be_none(self):
         self.residential_adress.individual = None
         self._assert_residential_adress_is_invalid()
-
 
     """Helper functions"""
 
@@ -127,7 +108,6 @@ class ResidentialAdressTests(TestCase):
             self.residential_adress.full_clean()
         except ValidationError:
             self.fail('Test individual is not valid.')
-
 
     # Assert a residential adress is invalid
     def _assert_residential_adress_is_invalid(self):
