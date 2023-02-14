033ba3e2ef9c29ebdcd83e1ee1c4486d8460a4af
AdnanTurkay <adntrky@gmail.com>
2023-02-14 13:26:56 +0000

    Fix login view tests.

--- vcpms/urls.py
+++ vcpms/urls.py
@@ -20,7 +20,8 @@ from portfolio import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.dashboard, name='dashboard'),
+    path('login', views.LogInCBV.as_view(), name='login'),
+    path('logout', views.log_out, name='logout'),
     path('search_result', views.searchcomp, name='search_result'),
-
     path('portfolio_company/', views.portfolio_company, name='portfolio_company'),
 ]
