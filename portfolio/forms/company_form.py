323a836af21584999e198e6965c6a9e69f119d6e
Rebi.boy <rebjok911@gmail.com>
2023-02-15 17:18:47 +0000

    created initial company form

---
+++ portfolio/forms/company_form.py
@@ -0,0 +1,14 @@
+"""Forms for the VC portfolio management site"""
+from django import forms
+from portfolio.models import Company
+
+
+# Form for creating an individual / client
+class CompanyCreateForm(forms.ModelForm):
+    class Meta:
+        model = Company
+        fields = ["name", "company_registration_number", "trading_names",
+                  "previous_names", "registered_address", "Email", "jurisdiction", "incorporation_date"]
+
+    def __init__(self, *args, **kwargs):
+        super(CompanyCreateForm, self).__init__(*args, **kwargs)
