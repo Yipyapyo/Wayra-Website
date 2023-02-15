3c937534ed8d4dc9e6443db7a63933933ea98bf4
yeungchinkan <y23@mail.com>
2023-02-15 22:07:58 +0000

    Create programme model
    - Create programme model file
    - Create mock portfolio company model

--- portfolio/models/programme_model.py
+++ portfolio/models/programme_model.py
@@ -1,11 +1,11 @@
 from django.db import models
 from portfolio.models import Individual, Company, Portfolio_Company
 
-class Programme(models.model):
+class Programme(models.Model):
     """Model for programmes"""
     name = models.CharField(max_length=255)
     cohort = models.PositiveIntegerField()
-    partners = models.ManyToManyField(Company)
-    participants = models.ManyToManyField(Portfolio_Company)
+    partners = models.ManyToManyField(Company, related_name="partners")
+    participants = models.ManyToManyField(Portfolio_Company, related_name="participants")
     coaches_mentors = models.ManyToManyField(Individual)
     
