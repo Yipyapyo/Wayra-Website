226febb3ba19fb5603aa282507548749005e04a2
AdnanTurkay <adntrky@gmail.com>
2023-02-15 12:56:05 +0000

    Refactor views.py

--- portfolio/views/individual_views.py
+++ portfolio/views/individual_views.py
@@ -1,24 +1,24 @@
 from django.shortcuts import render
-from .forms import IndividualCreateForm, AddressCreateForm
-from .models import Individual, ResidentialAddress
+from portfolio.forms import IndividualCreateForm, AddressCreateForm
+from portfolio.models import Individual, ResidentialAddress
 from django.shortcuts import redirect, render
 
-# Create your views here.
 
 """
 Create an individual.
 """
+
+
 def individual_create(request):
-    
     individualForm = IndividualCreateForm()
     addressForms = AddressCreateForm()
-    
+
     if request.method == "POST":
         individualForm = IndividualCreateForm(request.POST, prefix="form1")
         addressForms = AddressCreateForm(request.POST, prefix="form2")
         # addressForms = [AddressCreateForm(request.POST, prefix=str(x)) for x in range (0, 1)]
         if individualForm.is_valid() and addressForms.is_valid():
-        # if individualForm.is_valid() and all([addressForms.is_valid() for af in addressForms]):
+            # if individualForm.is_valid() and all([addressForms.is_valid() for af in addressForms]):
             new_individual = individualForm.save()
             new_address = addressForms.save(commit=False)
             new_address.individual = new_individual
@@ -27,23 +27,29 @@ def individual_create(request):
     else:
         individualForm = IndividualCreateForm(prefix="form1")
         addressForms = AddressCreateForm(prefix="form2")
-    
+
     context = {
         'individualForm': individualForm,
         'addressForms': addressForms,
     }
     return render(request, "individual_create.html", context=context)
 
+
 """
 List of individuals
 """
+
+
 def individual_page(request):
     data = {'object_list': Individual.objects.all()}
     return render(request, "individual_page.html", data)
 
+
 """
 Update a particular individual's information
 """
+
+
 def individual_update(request, id):
     individualForm = Individual.objects.get(id=id)
     addressForms = ResidentialAddress.objects.get(id=id)
@@ -65,9 +71,12 @@ def individual_update(request, id):
     }
     return render(request, 'individual_update.html', context=context)
 
+
 """
 Delete a particular individual
 """
+
+
 def individual_delete(request, id):
     individualForm = Individual.objects.get(id=id)
     if request.method == 'POST':
