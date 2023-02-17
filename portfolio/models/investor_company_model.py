3a6b9749e5d810a5ac10f471f3d3255a0ad35951
KRIYES MAHENDRA <kriyes.1.mahendra@kcl.ac.uk>
2023-02-17 15:00:58 +0000

    Add investor company model

---
+++ portfolio/models/investor_company_model.py
@@ -0,0 +1,63 @@
+from django.db import models
+from company_model import Company
+
+class InvestorCompany(Company):
+    VENTURE_CAPITAL = 'VC'
+    PRIVATE_EQUITY_FIRM = 'PEF'
+    ACCELERATOR = 'A'
+    INVESTMENT_PARTNER = 'IP'
+    CORPORATE_VENTURE_CAPITAL = 'CVC'
+    MICRO_VC = 'MVC'
+    ANGEL_GROUP = 'AG'
+    INCUBATOR = 'I'
+    INVESTMENT_BANK = 'IB'
+    FAMILY_INVESTMENT_OFFICE = 'FIO'
+    VENTURE_DEBT = 'VD'
+    CO_WORKING_SPACE = 'CWS'
+    FUND_OF_FUNDS = 'FOF'
+    HEDGE_FUND = 'HF'
+    GOVERNMENT_OFFICE = 'GO'
+    UNIVERSITY_PROGRAM = 'UP'
+    ENTREPRENEURSHIP_PROGRAM = 'EP'
+    SECONDARY_PURCHASER = 'SP'
+    STARTUP_COMPETITION = 'SC'
+    SYNDICATE = 'S'
+    PENSION_FUNDS = 'PF'
+
+    INVESTOR_TYPES = [
+        (VENTURE_CAPITAL, 'Venture Capital'),
+        (PRIVATE_EQUITY_FIRM, 'Private Equity Firm'),
+        (ACCELERATOR, 'Accelerator'),
+        (INVESTMENT_PARTNER, 'Investment Partner'),
+        (CORPORATE_VENTURE_CAPITAL, 'Corporate Venture Capital'),
+        (MICRO_VC, 'Micro VC'),
+        (ANGEL_GROUP, 'Angel Group'),
+        (INCUBATOR, 'Incubator'),
+        (INVESTMENT_BANK, 'Investment Bank'),
+        (FAMILY_INVESTMENT_OFFICE, 'Family Investment Office'),
+        (VENTURE_DEBT, 'Venture Debt'),
+        (CO_WORKING_SPACE), 'Co-Working Space',
+        (FUND_OF_FUNDS, 'Fund Of Funds'),
+        (HEDGE_FUND, 'Hedge Fund'),
+        (GOVERNMENT_OFFICE, 'Government Office'),
+        (UNIVERSITY_PROGRAM, 'University Program'),
+        (ENTREPRENEURSHIP_PROGRAM, 'Entrepreneurship Program'),
+        (SECONDARY_PURCHASER, 'Secondary Purchaser'),
+        (STARTUP_COMPETITION, 'Startup Competition'),
+        (SYNDICATE, 'Syndicate'),
+        (PENSION_FUNDS, ' Pension Funds'),
+    ]
+
+    AngelListLink = models.URLField("Angellist link", max_length=200)
+    CrunchbaseLink = models.URLField("Crunchbase link", max_length=200)
+    LinkedInLink = models.URLField("Linkedin link", max_length=200)
+    InvestedCompanies = models.ManyToManyField(Company, related_name="companies")
+    Type = models.CharField(
+        max_length=3,
+        choices=INVESTOR_TYPES,
+        default=VENTURE_CAPITAL,
+    )
+    NumberOfInvestments = models.PositiveIntegerField()
+    NumberOfLeadInvestments = models.PositiveIntegerField()
+    NumberOfDiversityInvestments = models.PositiveIntegerField()
+    NumberOfExits = models.PositiveIntegerField()
