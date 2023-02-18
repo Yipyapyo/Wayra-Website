d040c9c2c4c1d408e349e0a9ff32976c67241416
Chin Wan <k21016106@kcl.ac.uk>
2023-02-18 15:50:39 +0000

    Added one field to the investor individual model

--- portfolio/models/investor_individual_model.py
+++ portfolio/models/investor_individual_model.py
@@ -2,8 +2,8 @@ from django.db import models
 from individual_model import Individual
 
 class InvestorIndividual(Individual):
-
     NumberOfPortfolioCompanies = models.PositiveIntegerField()
     NumberOfPersonalInvestments = models.PositiveIntegerField()
     NumberOfPartnerInvestments = models.PositiveIntegerField()
+    PartOfIncubator = models.BooleanField(blank=False)
     NumberOfExits = models.PositiveIntegerField()
