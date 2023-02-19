e82ba0284acdc638445ae6e8d069c2f9722a4f4c
Chin Wan <k21016106@kcl.ac.uk>
2023-02-19 23:44:26 +0000

    Created the investment model for investment relations

---
+++ portfolio/models/investment_model.py
@@ -0,0 +1,25 @@
+from django.db import models
+from investor_company_model import InvestorCompany
+from company_model import Company
+from django.core.validators import MaxValueValidator
+from django.utils import timezone
+
+FOUNDING_ROUNDS = [
+    ('Seed round', 'Seed round'),
+    ('Series A', 'Series A'),
+    ('Series B', 'Series B'),
+    ('Series C', 'Series C'),
+    ('Coporate round', 'Coporate round'),
+    ('Convertible note', 'Convertible note'),
+    ('Venture round', 'Venture round'),
+    ('Debt financing', 'Debt financing'),
+    ('Post-IPO Equity', 'Post-IPO Equity')
+]
+
+class Investment(models.Model):
+    """Investment model for a investment from an investor to a startup"""
+    investor = models.ManyToManyField(InvestorCompany, related_name="investor")
+    startup = models.ManyToManyField(Company, related_name="startup")
+    typeOfFoundingRounds = models.CharField(max_length=50, choice=FOUNDING_ROUNDS)
+    moneyRaised = models.DecimalField("In millions", max_digits=5, decimal_places=2)
+    dateInvested = models.DateField(blank=False, validators=[MaxValueValidator(limit_value=timezone.now)])
