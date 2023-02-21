cb23ffd1cd4e226b1133bdefc48d8174041eddc8
KwanYuiChiu <k21003778@kcl.ac.uk>
2023-02-21 03:50:05 +0000

    Extract and rename search_programme from UpdateView

--- portfolio/views/programme_views.py
+++ portfolio/views/programme_views.py
@@ -7,10 +7,46 @@ from django.core.paginator import Paginator, EmptyPage
 from django.template.loader import render_to_string
 import logging
 from django.http import HttpResponse
-import json 
+import json
 from portfolio.forms import CreateProgrammeForm, EditProgrammeForm
 from portfolio.models import Programme
 from vcpms import settings
+
+
+def search_programme(request):
+    if request.method == "GET":
+
+        searched = request.GET['searchresult']
+
+        if (searched == ""):
+            search_result = {}
+        else:
+            search_result = Programme.objects.filter(name__contains=searched).values()
+
+        search_results_table_html = render_to_string('programmes/search/search_results_table.html', {
+            'search_results': list(search_result), 'searched': searched})
+
+        return HttpResponse(search_results_table_html)
+
+    elif request.method == "POST":
+        page_number = request.POST.get('page', 1)
+        searched = request.POST['searchresult']
+        if (searched == ""):
+            return redirect('programme_list')
+        else:
+            programmes = Programme.objects.filter(name__contains=searched).values()
+
+        paginator = Paginator(programmes, 6)
+        try:
+            programmes_page = paginator.page(page_number)
+        except EmptyPage:
+            programmes_page = []
+
+        return render(request, 'programmes/programme_list_page.html',
+                      {"programmes": programmes_page, "searched": searched})
+
+    else:
+        return HttpResponse("Request method is not a GET")
 
 
 class ProgrammeListView(LoginRequiredMixin, ListView):
@@ -44,41 +80,6 @@ class ProgrammeUpdateView(LoginRequiredMixin, UpdateView):
     def get_initial(self):
         return model_to_dict(self.get_object())
 
-    def searchprogramme(request):
-
-        if request.method == "GET":
-
-            searched = request.GET['searchresult']
-
-            if(searched == ""):
-                search_result = {}
-            else:
-                search_result = Programme.objects.filter(name__contains=searched).values()
-            
-            search_results_table_html = render_to_string('programmes/search/search_results_table.html', {
-            'search_results': list(search_result), 'searched':searched})
-
-            return HttpResponse(search_results_table_html)
-
-        elif request.method == "POST":
-            page_number = request.POST.get('page', 1)
-            searched = request.POST['searchresult']
-            if(searched == ""):
-                return redirect('programme_list')
-            else:
-                programmes = Programme.objects.filter(name__contains=searched).values()
-            
-            paginator = Paginator(programmes, 6)
-            try:
-                programmes_page = paginator.page(page_number)
-            except EmptyPage:
-                programmes_page = []
-
-            return render(request, 'programmes/programme_list_page.html', {"programmes": programmes_page, "searched":searched})
-
-        else:
-            return HttpResponse("Request method is not a GET")
-
 
 class ProgrammeDeleteView(LoginRequiredMixin, DeleteView):
     model = Programme
@@ -89,8 +90,8 @@ class ProgrammeDeleteView(LoginRequiredMixin, DeleteView):
     def get_success_url(self):
         return reverse('programme_list')
 
+
 class ProgrammeDetailView(LoginRequiredMixin, DetailView):
     model = Programme
     template_name = 'programmes/programme_page.html'
     pk_url_kwarg = 'id'
-
