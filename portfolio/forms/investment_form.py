495ad852e9502efcd999aa25c1e1f41ee2930f64
Chin Wan <k21016106@kcl.ac.uk>
2023-02-21 22:21:52 +0000

    Fixed problems

--- portfolio/forms/investment_form.py
+++ portfolio/forms/investment_form.py
@@ -3,16 +3,28 @@ from django import forms
 from portfolio.models.investment_model import Investment
 from portfolio.models.investor_individual_model import InvestorIndividual
 from portfolio.models.investor_company_model import InvestorCompany
+from portfolio.models.company_model import PortfolioCompany
 
 
 class InvestmentForm(forms.ModelForm):
-    """Form"""
     class Meta:
         model = Investment
-        fields = ['investor', 'startup', 'typeOfFoundingRounds', 'moneyRaised']
+        fields = ['investor', 'individualInvestor', 'startup', 'typeOfFoundingRounds', 'moneyRaised']
 
         investor = forms.ModelMultipleChoiceField(
             label="Select the investors in this investment",
             widget=forms.CheckboxSelectMultiple,
-            queryset=InvestorIndividual.objects.all().union(InvestorCompany.objects.all())
+            queryset=InvestorCompany.objects.all()
+        )
+
+        individualInvestor = forms.ModelMultipleChoiceField(
+            label="Select the individual investor in this investment if there is one",
+            widget=forms.CheckboxSelectMultiple,
+            queryset=InvestorIndividual.objects.all()
+        )
+
+        startup = forms.ModelMultipleChoiceField(
+            label="Select the portfolio company invested if there is one",
+            widget=forms.CheckboxSelectMultiple,
+            queryset=PortfolioCompany.objects.all()
         )
