972ad0bfea9e36333df37d3647abc82b1fd9b64f
Rebi.boy <rebjok911@gmail.com>
2023-02-22 18:23:14 +0000

    added pagination to archived companies table

--- portfolio/views/archive_views.py
+++ portfolio/views/archive_views.py
@@ -4,16 +4,25 @@ from django.http import HttpResponse
 from portfolio.models import Company, Individual
 from django.template.loader import render_to_string
 from django.urls import reverse
+from django.core.paginator import Paginator, EmptyPage
 
 """Archive views"""
 @login_required
 def archive(request):
     '''This ius the archive page. ONLY VIEWED BY ADMINS'''
+    page_number = request.GET.get('page', 1)
 
     companies = Company.objects.all()
 
+    paginator = Paginator(companies, 5)
+
+    try:
+        companies_page = paginator.page(page_number)
+    except EmptyPage:
+        companies_page = []
+
     context = {
-        "companies":companies,
+        "companies":companies_page,
         "search_url": reverse('archive_search'),
         "placeholder":"Search through the archive"
     }
