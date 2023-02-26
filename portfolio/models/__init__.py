f6108c0630a22c483b7452627a27769283fda55c
AdnanTurkay <adntrky@gmail.com>
2023-02-26 23:57:38 +0000

    Add document model to the namespace.

--- portfolio/models/__init__.py
+++ portfolio/models/__init__.py
@@ -8,3 +8,4 @@ from .investor_company_model import InvestorCompany
 from .investor_individual_model import InvestorIndividual
 from .past_experience_model import PastExperience
 from .programme_model import Programme
+from .document_model import Document
