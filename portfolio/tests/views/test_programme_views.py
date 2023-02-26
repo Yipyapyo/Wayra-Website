3908aef5b9e2c814932753bc8be6df98483257b3
Francis Yip <francisyip7@gmail.com>
2023-02-26 19:39:49 +0000

    written tests for programme views

--- portfolio/tests/views/test_programme_views.py
+++ portfolio/tests/views/test_programme_views.py
@@ -14,17 +14,25 @@ class ProgrammeViewTestCase(TestCase, LogInTester):
     ]
 
     def setUp(self) -> None:
-        self.url = reverse('dashboard')
+        self.url = reverse('programme_list')
         self.user = User.objects.get(email="john.doe@example.org")
         self.admin_user = User.objects.get(email="petra.pickles@example.org")
 
+    def test_create_programme_url(self):
+        url = reverse('programme_create')
+        self.assertEqual(url, '/programme_page/create/')
+    
     def test_programme_url(self):
-        self.assertEqual(self.url, '/programme_list')
+        self.assertEqual(self.url, '/programme_page/')
 
     def test_get_list(self):
         self.client.login(email=self.user.email, password="Password123")
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'programmes/programme_list.html')
+        self.assertTemplateUsed(response, 'programmes/programme_list_page.html')
 
+    def test_get_list_redirects_when_not_logged_in(self):
+        redirect_url = reverse_with_next('login', self.url)
+        response = self.client.get(self.url)
+        self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
