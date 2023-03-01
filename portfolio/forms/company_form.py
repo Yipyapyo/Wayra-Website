0d088b4477e3f9b02ee07ea38a382da846bc533e
yeungchinkan <y23@mail.com>
2023-03-01 21:00:46 +0000

    Create investor company create and edit form

--- portfolio/forms/company_form.py
+++ portfolio/forms/company_form.py
@@ -1,6 +1,7 @@
 """Forms for the VC portfolio management site"""
 from django import forms
-from portfolio.models import Company
+from portfolio.models import Company, InvestorCompany, Investment
+from django.db.models import Exists, OuterRef
 
 
 # Form for creating an individual / client
@@ -12,3 +13,25 @@ class CompanyCreateForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(CompanyCreateForm, self).__init__(*args, **kwargs)
+
+
+class CompanyChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj):
+        return obj.name
+
+# Form for setting company as investor company 
+class InvestorCompanyCreateForm(forms.ModelForm):
+    class Meta:
+        model = InvestorCompany
+        fields = ["company", "angelListLink", "crunchbaseLink", "linkedInLink", "classification"]
+
+    company = CompanyChoiceField(
+        queryset = Company.objects.filter(~Exists(Investment.objects.filter(investor=OuterRef('pk')))),
+        widget = forms.Select()
+    )
+
+class InvestorCompanyEditForm(forms.ModelForm):
+    class Meta:
+        model = InvestorCompany
+        fields = ["angelListLink", "crunchbaseLink", "linkedInLink", "classification"]
+    
