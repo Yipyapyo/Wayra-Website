726e771cd9148823cad735aac962dafe39e7c205
KwanYuiChiu <k21003778@kcl.ac.uk>
2023-03-03 22:04:34 +0000

    Refactor seeders into different files
    - Create Seeder interface
    - Create User,Company,Individual,InvestorCompany,PortfolioCompany,Investments,Programmes seeder

---
+++ portfolio/seeders/__init__.py
@@ -0,0 +1,7 @@
+from .seeder import *
+from .companies import *
+from .individuals import *
+from .users import *
+from .investments import *
+from .programmes import *
+from .portfolio_companies import *
---
+++ portfolio/seeders/investments.py
@@ -0,0 +1,63 @@
+import random
+
+from django.core.exceptions import ObjectDoesNotExist
+
+from portfolio.seeders import Seeder
+from portfolio.models import InvestorCompany, Company, Investment, Portfolio_Company
+from portfolio.models.investment_model import FOUNDING_ROUNDS
+
+
+class InvestorCompanySeeder(Seeder):
+    INVESTOR_COMPANY_COUNT = 5
+
+    def seed(self):
+        self._create_investor_companies(self.INVESTOR_COMPANY_COUNT)
+        print(f"{InvestorCompany.objects.count()} investor companies in the db.\n")
+
+    def _create_investor_companies(self, count):
+        for i in range(1, count + 1):
+            try:
+                InvestorCompany.objects.get(id=i)
+                print(f"InvestorCompany with id({i}) has already seeded.")
+            except ObjectDoesNotExist:
+                InvestorCompany.objects.create(
+                    company=Company.objects.get(id=i),
+                    angelListLink="https://www.Angelist.com",
+                    crunchbaseLink="https://www.crunchbase.com/",
+                    linkedInLink="https://www.linkedin.com/",
+                    classification=InvestorCompany.INVESTOR_TYPES[i],
+                )
+                print(f"InvestorCompany with id({i}) has been seeded.")
+
+
+class InvestmentSeeder(Seeder):
+    INVESTMENT_COUNT = 25
+
+    def seed(self):
+        self._create_investments(self.INVESTMENT_COUNT)
+        print(f"{Investment.objects.count()} investments in the db.\n")
+
+    def _get_objects_from_models(self, model, index, slice_size):
+        random_number_of_object = random.randint(1, slice_size)
+        objects = list(
+            model.objects.filter(id__in=range(index * slice_size, index * slice_size + random_number_of_object)))
+        return objects
+
+    def _create_investments(self, count):
+        for i in range(1, count + 1):
+            try:
+                Investment.objects.get(id=i)
+                print(f"InvestorCompany with id({i}) has already seeded.")
+            except ObjectDoesNotExist:
+                company_count = 1
+                Investment.objects.create(
+                    investor=InvestorCompany.objects.get(id=company_count),
+                    startup=Portfolio_Company.objects.get(wayra_number=f'WN-{i}'),
+                    typeOfFoundingRounds=random.choices(FOUNDING_ROUNDS),
+                    investmentAmount=random.randint(10_000, 10_000_000),
+                    dateInvested=self.faker.date_this_century(),
+                )
+                if random.randint(0, 2) > 0:
+                    company_count += 1
+
+                print(f"Investment with id({i}) has been seeded.")
