27e7aef02a4b0015633dfef660ace027f639932b
Fergan Yalim <fergan.yalim@kcl.ac.uk>
2023-03-03 22:49:43 +0000

    .

--- portfolio/tests/views/test_investor_individual_create_view.py
+++ portfolio/tests/views/test_investor_individual_create_view.py
@@ -1,4 +1,4 @@
-from asyncio.windows_events import NULL
+
 from django.test import TestCase
 from django.urls import reverse
 from portfolio.models import ResidentialAddress, PastExperience, InvestorIndividual, User
