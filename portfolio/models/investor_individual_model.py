af19ce89c037bd8eea28980cb2f054c631192107
Rebi.boy <rebjok911@gmail.com>
2023-03-10 20:00:12 +0000

    differentiated between subclass models of Individual

--- portfolio/models/investor_individual_model.py
+++ portfolio/models/investor_individual_model.py
@@ -1,9 +1,10 @@
 from django.db import models
-from portfolio.models.individual_model import Individual
+from portfolio.models.individual_model import Individual, IndividualManager
 
 
 class InvestorIndividual(Individual):
     """An investor in a company."""
+    objects = IndividualManager()
 
     NumberOfPortfolioCompanies = models.PositiveIntegerField()
     NumberOfPersonalInvestments = models.PositiveIntegerField()
