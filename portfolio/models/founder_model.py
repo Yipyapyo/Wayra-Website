5de644a9e98fa7eb3d573c1b9c45539c6ef90ff2
Chin Wan <k21016106@kcl.ac.uk>
2023-03-10 16:20:48 +0000

    refactored founder model.

--- portfolio/models/founder_model.py
+++ portfolio/models/founder_model.py
@@ -1,9 +1,11 @@
 from django.db import models
 from portfolio.models.individual_model import Individual
-# from portfolio.models.company_model import Company
+from portfolio.models.company_model import Company
 
 
-class Founder(Individual):
+class Founder(models.Model):
     """A founder of a company."""
-    companyFounded = models.CharField(max_length=100, blank=False, default="startup")
-    additionalInformation = models.CharField(max_length=500, blank=True)
+    # companyFounded = models.CharField(max_length=100, blank=False, default="startup")
+    companyFounded = models.OneToOneField(Company, on_delete=models.CASCADE)
+    individualFounder = models.OneToOneField(Individual, on_delete=models.CASCADE)
+
