7349c4e12e44abc3381e49a6d55fc22eee652325
AdnanTurkay <adntrky@gmail.com>
2023-03-16 12:54:49 +0000

    Add document and URL upload functionality.

--- portfolio/forms/document_form.py
+++ portfolio/forms/document_form.py
@@ -1,10 +1,49 @@
 from django import forms
-from portfolio.models import Document
+from portfolio.models import Document, Company
+from django.utils.translation import gettext_lazy as _
 
 
-class DocumentForm(forms.ModelForm):
-    """Form for storing a document."""
+class DocumentUploadForm(forms.ModelForm):
+    """A form for storing a document."""
 
     class Meta:
         model = Document
-        fields = ["file_name", "file_type", "file_size", "url", "file", "company"]
+        fields = ["file"]
+        exclude = ["file_name", "file_type", "file_size", "url", "company", "created_at", "updated_at"]
+        labels = {
+            "file": _("Select a file to upload:")
+        }
+
+    def save(self, commit=True):
+        document = super().save(commit=False)
+        document.file_name = self.cleaned_data["file"].name
+        document.file_type = self.cleaned_data["file"].name.split(".")[-1]
+        document.file_size = self.cleaned_data["file"].size
+
+        if commit:
+            document.save()
+
+        return document
+
+
+class URLUploadForm(forms.ModelForm):
+    """A form for storing an URL."""
+
+    class Meta:
+        model = Document
+        fields = ["file_name", "url"]
+        exclude = ["file_type", "file_size", "file", "company", "created_at", "updated_at"]
+        labels = {
+            "file_name": _("File name:"),
+            "url": _("URL:")
+        }
+
+    def save(self, commit=True):
+        document = super().save(commit=False)
+        document.file_name = self.cleaned_data["file_name"]
+        document.file_type = "URL"
+
+        if commit:
+            document.save()
+
+        return document
--- portfolio/views/document_views.py
+++ portfolio/views/document_views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, Http404
-from portfolio.models import Document
 from django.shortcuts import render, redirect
+from portfolio.forms import DocumentUploadForm, URLUploadForm
+from portfolio.models import Document
 
 import os
 
@@ -9,7 +10,35 @@ import os
 # Render the document upload page.
 @login_required
 def document_upload(request, company_id):
-    return render(request, "document/document_upload.html", {"company_id": company_id})
+    if request.method == "POST":
+        if "upload_file" in request.POST:
+            form = DocumentUploadForm(request.POST, request.FILES)
+
+            if form.is_valid():
+                new_document = form.save(commit=False)
+                new_document.company_id = company_id
+                new_document.save()
+                return redirect("portfolio_company", company_id=company_id)
+
+        elif "upload_url" in request.POST:
+            form = URLUploadForm(request.POST)
+
+            if form.is_valid():
+                new_document = form.save(commit=False)
+                new_document.company_id = company_id
+                new_document.save()
+                return redirect("portfolio_company", company_id=company_id)
+    else:
+        file_form = DocumentUploadForm()
+        url_form = URLUploadForm()
+
+        context = {
+            "file_form": file_form,
+            "url_form": url_form,
+            "company_id": company_id
+        }
+
+    return render(request, "document/document_upload.html", context)
 
 
 # Redirect to an external document.
@@ -31,6 +60,7 @@ def download_document(request, file_id):
         with open(file_path, "rb") as download_file:
             response = HttpResponse(download_file.read(), content_type='application/octet-stream')
             response['Content-Disposition'] = 'attachment; filename=' + document.file_name
+
             return response
     else:
         raise Http404
