b67b2e5376cb368d34bfac8e8d9159c7f134911c
KwanYuiChiu <k21003778@kcl.ac.uk>
2023-03-19 21:28:26 +0000

    Update dashboard_views.py
    -comment out print statement

--- portfolio/views/dashboard_views.py
+++ portfolio/views/dashboard_views.py
@@ -20,17 +20,17 @@ def dashboard(request):
 
     # Data for the each company will be listed here.
     page_number = request.GET.get('page', 1)
-    print(type(request.session.get('company_filter')))
+    # print(type(request.session.get('company_filter')))
 
     if int(request.session['company_filter']) == 3:
-        print("A")
+        # print("A")
         investors = Investor.objects.all()
         companies = Company.objects.filter(id__in=investors.values('company'), is_archived=False).order_by('id')
     elif int(request.session['company_filter']) == 2:
-        print("B")
+        # print("B")
         companies =Company.objects.filter(parent_company__parent_company__is_archived=False).order_by('id')
     else:
-        print("C")
+        # print("C")
         companies = Company.objects.filter(is_archived=False).order_by('id')
 
     paginator = Paginator(companies, 6)
