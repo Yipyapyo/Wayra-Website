679b480e8388d9fb3e51ee07c8a6cd8c7414113c
Francis Yip <francisyip7@gmail.com>
2023-03-22 15:41:58 +0000

    finish testing on document views

--- portfolio/tests/views/test_document_views.py
+++ portfolio/tests/views/test_document_views.py
@@ -22,6 +22,7 @@ class DocumentViewsTestCase(TestCase):
 
     fixtures = ["portfolio/tests/fixtures/default_company.json",
                 'portfolio/tests/fixtures/default_user.json',
+                'portfolio/tests/fixtures/other_users.json'
                 ]
 
 
@@ -57,8 +58,6 @@ class DocumentViewsTestCase(TestCase):
             except IOError:
                 time.sleep(.1)
         
-
-    
     def test_document_upload_url(self):
         self.assertEqual(self.url, f'/portfolio_company/{self.defaultCompany.id}/upload_document/')
 
@@ -92,20 +91,78 @@ class DocumentViewsTestCase(TestCase):
         after_count = Document.objects.count()
         self.assertEqual(before_count, after_count-1)
 
-    # def test_download_document(self):
-    #     self.client.login(email=self.user.email, password="Password123")
-    #     document = Document.objects.get(id=1)
-    #     self.url = reverse('download_document', kwargs={'file_id': Document.id})
-    #     response = self.client.get()
+    def test_download_document_url(self):
+        self.client.login(email=self.user.email, password="Password123")
+        self.url = reverse('download_document', kwargs={'file_id': 1})
+        self.assertEqual(self.url, f'/download_document/1')
+
+    def test_download_document(self):
+        self.client.login(email=self.user.email, password="Password123")
+        self.client.post(self.url, self.document_form_input, follow=True)
+        document = Document.objects.get(file_id=1)
+        self.url = reverse('download_document', kwargs={'file_id': document.file_id})
+        response = self.client.get(self.url)
+        self.assertEquals(
+            response.get('Content-Disposition'),
+            "attachment; filename=TestingExcel.xlsx"
+        )
+
+    def test_document_download_redirects_when_not_logged_in(self):
+        self.url = reverse('download_document', kwargs={'file_id': 1})
+        redirect_url = reverse_with_next('login', self.url)
+        response = self.client.get(self.url)
+        self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
     
-    # def test_delete_document_url(self):
-        # self.assertEqual(self.url, f'/programme_page/{self.target_programme.id}/delete/')
+    def test_delete_document_url(self):
+        self.client.login(email=self.user.email, password="Password123")
+        self.url = reverse('delete_document', kwargs={'file_id': 1})
+        self.assertEqual(self.url, f'/delete_document/1')
 
-    # def test_delete_document(self):
+    def test_delete_document(self):
+        self.client.login(email=self.user.email, password="Password123")
+        self.client.post(self.url, self.document_form_input, follow=True)
+        before_count = Document.objects.count()
+        self.url = reverse('delete_document', kwargs={'file_id': 1})
+        self.client.get(self.url)
+        after_count = Document.objects.count()
+        self.assertEqual(before_count, after_count+1)
 
-    # # def test_document_delete_redirects_when_not_logged_in(self):
-    #     redirect_url = reverse_with_next('login', self.url)
-    #     response = self.client.get(self.url)
-    #     self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
+    def test_document_delete_redirects_when_not_logged_in(self):
+        self.url = reverse('delete_document', kwargs={'file_id': 1})
+        redirect_url = reverse_with_next('login', self.url)
+        response = self.client.get(self.url)
+        self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
-    # def test_document_change_permissions(self):
+    def test_document_change_permissions_url(self):
+        self.client.login(email=self.user.email, password="Password123")
+        self.url = reverse('change_permissions', kwargs={'file_id': 1})
+        self.assertEqual(self.url, f'/document_permissions/1')
+    
+    def test_document_change_permissions(self):
+        self.client.login(email=self.user.email, password="Password123")
+        self.client.post(self.url, self.document_form_input, follow=True)
+        before_change_permissions = Document.objects.get(file_id=1).is_private
+        self.url = reverse('change_permissions', kwargs={'file_id': 1})
+        self.client.get(self.url)
+        after_change_permissions = Document.objects.get(file_id=1).is_private
+        self.assertNotEqual(before_change_permissions, after_change_permissions)
+                
+    def test_document_change_permissions_redirects_when_not_logged_in(self):
+        self.url = reverse('change_permissions', kwargs={'file_id': 1})
+        redirect_url = reverse_with_next('login', self.url)
+        response = self.client.get(self.url)
+        self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)    
+
+    def test_open_url(self):
+        self.client.login(email=self.user.email, password="Password123")
+        self.client.post(self.url, self.url_form_data, follow=True)
+        self.url = reverse('open_url', kwargs={'file_id': 1})
+        response = self.client.get(self.url)
+        redirect_url = "https://www.wayra.uk"
+        self.assertEqual(response['Location'], redirect_url)
+
+    def test_document_open_url_redirects_when_not_logged_in(self):
+        self.url = reverse('open_url', kwargs={'file_id': 1})
+        redirect_url = reverse_with_next('login', self.url)
+        response = self.client.get(self.url)
+        self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
